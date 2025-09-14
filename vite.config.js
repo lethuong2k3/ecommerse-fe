239107559daea8b9b58fbbc5207f3f58489d5eba
lethuong2k3 <leanhthuong2k3@gmail.com>
2025-09-14 15:20:25 +0700

    update config vite

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,22 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/',
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src'),
-            '@components': path.resolve(__dirname, './src/components'),
-            '@styles': path.resolve(__dirname, './src/assets/styles'),
-            '@images': path.resolve(__dirname, './src/assets/images'),
-            '@icons': path.resolve(__dirname, './src/assets/icons'),
-            '@apis': path.resolve(__dirname, './src/apis'),
-            '@routers': path.resolve(__dirname, './src/routers'),
-            '@hooks': path.resolve(__dirname, './src/hooks'),
-            '@contexts': path.resolve(__dirname, './src/contexts'),
-            '@pages': path.resolve(__dirname, './src/pages'),
+            '@': path.resolve(__dirname, 'src'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@styles': path.resolve(__dirname, 'src/assets/styles'),
+            '@images': path.resolve(__dirname, 'src/assets/images'),
+            '@icons': path.resolve(__dirname, 'src/assets/icons'),
+            '@apis': path.resolve(__dirname, 'src/apis'),
+            '@routers': path.resolve(__dirname, 'src/routers'),
+            '@hooks': path.resolve(__dirname, 'src/hooks'),
+            '@contexts': path.resolve(__dirname, 'src/contexts'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
         },
+    },
+    build: {
+        outDir: 'dist',
     },
 });
