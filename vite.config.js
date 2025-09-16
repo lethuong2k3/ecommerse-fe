4a721b01ec6c49f72abdf0f4842c195f96e8ea9e
lethuong2k3 <leanhthuong2k3@gmail.com>
2025-09-16 10:53:36 +0700

    update config vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        watch: {
+            usePolling: true,
+        },
+    },
     plugins: [react(), tsconfigPaths()],
     resolve: {
         alias: {
